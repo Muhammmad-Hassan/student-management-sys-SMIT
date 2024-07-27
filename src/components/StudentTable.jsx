8307e29d3d91ae71e2f88e202edef25b8945d497
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-07-27 11:22:23 -0700

    .env added

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,4 +1,3 @@
-
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -10,17 +9,17 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
-import UpdateStudent from './UpdateStudent'
+import UpdateStudent from "./UpdateStudent";
 import { useState } from "react";
 
 export default function StudentTable({ student, setStudent }) {
-  const [editDilogOpen, setEditDilogOpen] = useState(false)
-  const [currentStudent, setCurrentStudent] = useState(null)
+  const [editDilogOpen, setEditDilogOpen] = useState(false);
+  const [currentStudent, setCurrentStudent] = useState(null);
   // updatedStudent
   const handleUpdateStudent = (studntId) => {
-    const studen =  student.find(s => s.id === studntId)
-    setCurrentStudent(studen)
-    setEditDilogOpen(true)
+    const studen = student.find((s) => s.id === studntId);
+    setCurrentStudent(studen);
+    setEditDilogOpen(true);
   };
   // DeleteStudent
   const handleDeleteStudent = async (studntId) => {
@@ -29,81 +28,82 @@ export default function StudentTable({ student, setStudent }) {
     setStudent(student.filter((studnt) => studnt.id !== studntId));
   };
 
-  // close update dilog 
+  // close update dilog
   function handleDilogClosed() {
-    setEditDilogOpen(false)
-    setCurrentStudent(null)
+    setEditDilogOpen(false);
+    setCurrentStudent(null);
   }
 
-   async function handleSaveStudent(){
-   const studentDoc =  doc(db, 'students', currentStudent.id)
-    await updateDoc(studentDoc,{
-     name: currentStudent.name,
-     age: currentStudent.age
-   })
-   setStudent(student.map((studen) => studen.id === currentStudent.id ? 
-   currentStudent : studen))
-   handleDilogClosed();
+  async function handleSaveStudent() {
+    const studentDoc = doc(db, "students", currentStudent.id);
+    await updateDoc(studentDoc, {
+      name: currentStudent.name,
+      age: currentStudent.age,
+    });
+    setStudent(
+      student.map((studen) =>
+        studen.id === currentStudent.id ? currentStudent : studen
+      )
+    );
+    handleDilogClosed();
   }
-  // handlechange dilog 
-  function handleChange(e){
-    const {name , value} = e.target
-    setCurrentStudent((prev) =>({
+  // handlechange dilog
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setCurrentStudent((prev) => ({
       ...prev,
-      [name] : value
-    }))
-    console.log(e.target)
-
+      [name]: value,
+    }));
+    console.log(e.target);
   }
 
   return (
-
     <>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Student Roll #</TableCell>
-            <TableCell align="center">Student Name</TableCell>
-            <TableCell align="center">Student Age</TableCell>
-            <TableCell align="center">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {student.map((studnt) => (
-            <TableRow
-              key={studnt.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="center">{studnt.rollNo}</TableCell>
-              <TableCell align="center">{studnt.name}</TableCell>
-              <TableCell align="center">{studnt.age}</TableCell>
-              <TableCell align="center">
-                <EditIcon
-                  style={{
-                    cursor: "pointer",
-                    color: "#007bff",
-                    marginRight: "10",
-                  }}
-                  onClick={() => handleUpdateStudent(studnt.id)}
-                />
-                <DeleteIcon
-                  style={{ cursor: "pointer", color: "crimson" }}
-                  onClick={() => handleDeleteStudent(studnt.id)}
-                />
-              </TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Student Roll #</TableCell>
+              <TableCell align="center">Student Name</TableCell>
+              <TableCell align="center">Student Age</TableCell>
+              <TableCell align="center">Action</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <UpdateStudent 
-    editDilogOpen={editDilogOpen} 
-    currentStudent={currentStudent} 
-    handleDilogClosed={handleDilogClosed} 
-    handleChange={handleChange}
-    handleSaveStudent = {handleSaveStudent}
-    />
+          </TableHead>
+          <TableBody>
+            {student.map((studnt) => (
+              <TableRow
+                key={studnt.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">{studnt.rollNo}</TableCell>
+                <TableCell align="center">{studnt.name}</TableCell>
+                <TableCell align="center">{studnt.age}</TableCell>
+                <TableCell align="center">
+                  <EditIcon
+                    style={{
+                      cursor: "pointer",
+                      color: "#007bff",
+                      marginRight: "10",
+                    }}
+                    onClick={() => handleUpdateStudent(studnt.id)}
+                  />
+                  <DeleteIcon
+                    style={{ cursor: "pointer", color: "crimson" }}
+                    onClick={() => handleDeleteStudent(studnt.id)}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <UpdateStudent
+        editDilogOpen={editDilogOpen}
+        currentStudent={currentStudent}
+        handleDilogClosed={handleDilogClosed}
+        handleChange={handleChange}
+        handleSaveStudent={handleSaveStudent}
+      />
     </>
   );
 }
